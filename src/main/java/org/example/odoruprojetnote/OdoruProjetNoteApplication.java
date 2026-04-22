bdbc14e7abc8d4b030ac44db32b1ff19a5427d95
clarabascoul <clara.bascoul.31@gmail.com>
2026-04-22 15:09:20 +0200

    Commit initialisation de l'application

---
+++ src/main/java/org/example/odoruprojetnote/OdoruProjetNoteApplication.java
@@ -0,0 +1,13 @@
+package org.example.odoruprojetnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoruProjetNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdoruProjetNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/odoruprojetnote/OdoruProjetNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.odoruprojetnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruProjetNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
